33712f2839db9b4ac4f0e277830e8273a5b0d3da
kzmw <63456328+kzmw@users.noreply.github.com>
2021-06-03 21:49:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function map(){
   const map_name = document.getElementById("map_select").value;
   if (map_name == "The Airship"){
-    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/the_airship.png" alt="The Airshipのマップ">';
+    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/the_airship.png" style="zoom: 60%;" alt="The Airshipのマップ">';
   }
   else if (map_name == "The Skeld"){
-    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/the_skeld.png" alt="The Skeldのマップ">';
+    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/the_skeld.png" style="zoom: 60%;" alt="The Skeldのマップ">';
   }
   else if (map_name == "MIRA HQ"){
-    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/mira_hq.png" alt="MIRA HQのマップ">';
+    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/mira_hq.png" style="zoom: 60%;" alt="MIRA HQのマップ">';
   }
   else if (map_name == "POLUS"){
-    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/polus.png" alt="POLUSのマップ">';
+    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/polus.png" style="zoom: 60%;" alt="POLUSのマップ">';
   }
   else if (map_name == "blank"){
     document.getElementById("map_field").innerHTML = '';
