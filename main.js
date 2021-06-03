87a4167813ea461b5080daafff9fa01bb7f661a5
kzmw <63456328+kzmw@users.noreply.github.com>
2021-06-03 21:33:46 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function map(){
+  const map_name = document.getElementById("map_select").value;
+  if (map_name == "The Airship"){
+    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/the_airship.png" alt="The Airshipのマップ">';
+  }
+  else if (map_name == "The Skeld"){
+    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/the_skeld.png" alt="The Skeldのマップ">';
+  }
+  else if (map_name == "MIRA HQ"){
+    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/mira_hq.png" alt="MIRA HQのマップ">';
+  }
+  else if (map_name == "POLUS"){
+    document.getElementById("map_field").innerHTML = '<img border="0" src="./img/polus.png" alt="POLUSのマップ">';
+  }
+}
